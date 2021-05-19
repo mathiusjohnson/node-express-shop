1e5d3e4c24d72fbc7a4108c50fcd67d250271911
Mathius <devachievem@gmail.com>
2021-05-18 20:37:56 -0700

    remove sequelize cart, add user and product class models

--- routes/admin.js
+++ routes/admin.js
@@ -3,6 +3,7 @@ const path = require('path');
 const express = require('express');
 
 const adminController = require('../controllers/admin');
+const { mongoConnect } = require('../util/database');
 
 const router = express.Router();
 
--- util/database.js
+++ util/database.js
@@ -1,8 +1,44 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node_complete', 'root', 'RU4realio', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+// const mongoConnect = (callback) => { 
+//   MongoClient.connect('mongodb+srv://mathiusj:RU4realio@cluster0.c35hv.mongodb.net/node_complete?retryWrites=true&w=majority')
+//     .then(client => {
+//       console.log('Connected!');
+//       _db = client.db();
+//       callback(client);
+//     })
+//     .catch(err => {
+//       console.log(err)
+//       throw err;
+//     })
+
+// }
+// mongodb+srv://mathiusj:<password>@cluster0.c35hv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// 
+
+const mongoConnect = callback => {
+  MongoClient.connect('mongodb+srv://mathiusj:RU4realio@cluster0.c35hv.mongodb.net/node_complete?retryWrites=true&w=majority')
+
+    .then(client => {
+      console.log('Connected!');
+      _db = client.db();
+      callback();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+
+  throw 'No database found!'
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
