f52555cf16d6dc7acf87385b78bae480172b299e
Mathius <devachievem@gmail.com>
2021-05-18 21:46:37 -0700

    implement mongodb

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('60a4861758f0b2339ef1242d')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id);
       next();
     })
     .catch(err => console.log(err));
